e546af7ff636b79f8b7f14c694b9e7e0bf2ab360
Dat2807 <lydat28072004@gmail.com>
2025-09-27 21:18:29 +0700

    Add Dog01 and projectile animations and prefabs
    Introduced animation assets and controllers for Dog01 and projectiles, including attack, dead, idle, and walk animations. Added corresponding prefabs and scripts for dogs, chickens, projectiles, and walls. Refactored project structure by moving assets into the 'Game' directory for better organization.

---
+++ Assets/Game/Scripts/Wall/Wall.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Game.Characters.Walls
+{
+    public class Wall : MonoBehaviour
+    {
+        [Header("Wall Stats")]
+        [Tooltip("Máu tối đa của tường")]
+        public int maxHP = 30;
+
+        public int CurrentHP { get; private set; }
+
+        private void Awake()
+        {
+            CurrentHP = maxHP;
+        }
+
+        public void TakeDamage(int damage)
+        {
+            CurrentHP -= damage;
+            
+            if (CurrentHP <= 0)
+            {
+                Die();
+            }
+        }
+
+        private void Die()
+        {          
+            Destroy(gameObject);
+        }
+    }
+}
